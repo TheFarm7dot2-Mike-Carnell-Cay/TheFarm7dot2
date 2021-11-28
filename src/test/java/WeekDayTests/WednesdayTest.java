86b53f899c00bea08277a793c26e60c7ebcff927
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-28 17:37:02 -0500

    wednesday done

--- src/test/java/WeekDayTests/WednesdayTest.java
+++ src/test/java/WeekDayTests/WednesdayTest.java
@@ -3,9 +3,7 @@ package WeekDayTests;
 import Person.Farmer;
 import Person.Froilan;
 import Person.Froilanda;
-import com.zipcodewilmington.froilansfarm.Animals.Egg;
-import com.zipcodewilmington.froilansfarm.Animals.Horse;
-import com.zipcodewilmington.froilansfarm.Animals.Stable;
+import com.zipcodewilmington.froilansfarm.Animals.*;
 import com.zipcodewilmington.froilansfarm.Crops.earCorn;
 import com.zipcodewilmington.froilansfarm.Crops.tomato;
 import org.junit.Assert;
@@ -79,6 +77,26 @@ public class WednesdayTest {
             testFarmer.feedAnimals(testHorse, testCorn);
             testFarmer.feedAnimals(testHorse, testCorn);
             int actual = testHorse.getAmountEaten();
+            //then
+            Assert.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public  void testFeedingChicken() {
+        //given
+        Farmer testFarmer = new Froilan();
+        earCorn testCorn = new earCorn();
+        ChickenCoop testCoop = new ChickenCoop();
+        testCoop.addChickens(15);
+        int expected = 3;
+        for (int i = 0; i < 15; i++) {
+            Chicken testChicken = testCoop.getChicken(i);
+            //when
+            testFarmer.feedAnimals(testChicken, testCorn);
+            testFarmer.feedAnimals(testChicken, testCorn);
+            testFarmer.feedAnimals(testChicken, testCorn);
+            int actual = testChicken.getFoodEaten();
             //then
             Assert.assertEquals(expected, actual);
         }
