de49a45522782b8c7965f281f1f1dca0f3ba5131
mgawron8 <92128861+mgawron8@users.noreply.github.com>
2021-11-27 21:32:24 -0500

    current progress

--- src/main/java/com/zipcodewilmington/froilansfarm/Animals/Stable.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animals/Stable.java
@@ -1,9 +1,19 @@
 package com.zipcodewilmington.froilansfarm.Animals;
 
 public class Stable {
+    public Horse[] stable1 = new Horse[4];
+    public Horse[] stable2 = new Horse[4];
+    public Horse[] stable3 = new Horse[2];
 
-    public Horse[] createStable(Integer Horses){
-        Horse[] stable = new Horse[Horses];
-        return stable;
+    public void assignHorses(){
+        for (int i = 0; i < 4; i++) {
+            stable1[i] = new Horse();
+        }
+        for (int i = 0; i < 4; i++) {
+            stable2[i] = new Horse();
+        }
+        for (int i = 0; i < 2; i++) {
+            stable3[i] = new Horse();
+        }
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Crops/CropRow.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Crops/CropRow.java
@@ -5,11 +5,11 @@ import com.zipcodewilmington.froilansfarm.Edible;
 import java.util.ArrayList;
 
 public class CropRow extends Crop{
-    ArrayList<Crop> spots = new ArrayList<Crop>();
+    ArrayList<Crop> plantSpots = new ArrayList<Crop>();
 
 public void addCrop(Crop crop) {
     for (int i = 0; i < 5; i++) {
-        spots.add(crop);
+        plantSpots.add(crop);
     }
 }
 
