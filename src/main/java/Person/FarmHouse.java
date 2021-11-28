002be9c4d6c265974cfc9c65fe3db12dce054248
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-28 15:11:11 -0500

    added farmhouse tests

--- src/main/java/Person/FarmHouse.java
+++ src/main/java/Person/FarmHouse.java
@@ -1,6 +1,8 @@
 package Person;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FarmHouse {
     private final ArrayList<Person> listOfPeople = new ArrayList<Person>();
@@ -8,11 +10,15 @@ public class FarmHouse {
     public FarmHouse() {
     }
 
-    public void addPerson(){
-
+    public void addPerson(Person person){
+        listOfPeople.add(person);
     }
 
-    public void kickPersonOut() {
+    public void kickPersonOut(Person person) {
+        listOfPeople.remove(person);
+    }
 
+    public List<Person> getListOfPeople() {
+        return listOfPeople;
     }
 }
